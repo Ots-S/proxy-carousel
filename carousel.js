98b89982f977eca4666e95216a7b378fa0befd49
Seb Ots <ots.sebastien@gmail.com>
2020-10-28 09:11:17 +0100

    fix - fetch

--- carousel.js
+++ carousel.js
@@ -1,28 +1,10 @@
 let dataApi = [];
-
-async function fetchDatas() {
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-  const url = "http://62.210.247.201:9000/test";
-  const response = await fetch(proxyUrl + url);
-  dataApi = await response.json();
-}
-
-fetchDatas();
-
 const numberOfImages = 7;
 const indexes = [];
 for (let i = 0; i <= numberOfImages - 1; i++) {
   indexes.push(i);
 }
 const time = 1200;
-
-function checkAndIncrement(index) {
-  if (index < dataApi.length - 1) {
-    return index + 1;
-  } else {
-    return 0;
-  }
-}
 
 function changeImg() {
   for (let i = 0; i <= indexes.length - 1; i++) {
@@ -34,6 +16,20 @@ function changeImg() {
   setTimeout("changeImg()", time);
 }
 
-window.onload = setTimeout(function () {
+function checkAndIncrement(index) {
+  if (index < dataApi.length - 1) {
+    return index + 1;
+  } else {
+    return 0;
+  }
+}
+
+async function fetchDatas() {
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const url = "http://62.210.247.201:9000/test";
+  const response = await fetch(proxyUrl + url);
+  dataApi = await response.json();
   changeImg();
-}, 500);
+}
+
+fetchDatas();
